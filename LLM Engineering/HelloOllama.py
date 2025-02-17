2de6744af94d6297ac5fc965ce82665decde5c18
Atchuta Vani <vani.c005@gmail.com>
2025-02-16 17:44:48 -0800

    Create HelloOllama.py

---
+++ LLM Engineering/HelloOllama.py
@@ -0,0 +1,23 @@
+#Making a call to Open Source LLM OLLAMA and print response.
+
+import requests
+import ollama
+from bs4 import BeautifulSoup
+from IPython.display import Markdown, display
+# Constants
+
+OLLAMA_API = "http://localhost:11434/api/chat"
+HEADERS = {"Content-Type": "application/json"}
+
+MODEL = "llama3.2"
+
+messages = [
+    {"role": "user", "content": "Describe some of the business applications of Generative AI"}
+]
+payload = {
+        "model": MODEL,
+        "messages": messages,
+        "stream": False}
+    
+response = ollama.chat(model=MODEL, messages=messages)
+print(response['message']['content'])
